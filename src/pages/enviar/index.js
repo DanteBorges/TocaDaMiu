e5de69afc9c66608feeed53548fa5eb632f90b31
Daniel <danielnorges@gmail.com>
2022-12-06 22:36:36 -0300

    remove usestate

--- src/pages/enviar/index.js
+++ src/pages/enviar/index.js
@@ -1,11 +1,13 @@
 import "./style.css";
-import React from 'react';
-import { useForm, ValidationError } from '@formspree/react';
+import React from "react";
+import { useForm } from "@formspree/react";
 
 const Enviar = () => {
   const [state, handleSubmit] = useForm("xdojbaek");
   if (state.succeeded) {
-    return <p>Obrigado por enviar!! Vamos analisar o envio e retornamos a você.</p>;
+    return (
+      <p>Obrigado por enviar!! Vamos analisar o envio e retornamos a você.</p>
+    );
   }
   return (
     <div>
@@ -13,24 +15,66 @@ const Enviar = () => {
         <div class="EnviarInfo">
           <h1>Você deseja ajudar sugerindo uma ferramenta?</h1>
           <h3>
-            Preencha o formulário abaixo com os dados requisitados que analisaremos a sua sugestão!
+            Preencha o formulário abaixo com os dados requisitados que
+            analisaremos a sua sugestão!
           </h3>
         </div>
         <form onSubmit={handleSubmit}>
+          <label class="label" for="full-name">
+            Nome Completo
+          </label>
+          <input
+            class="input"
+            type="text"
+            name="name"
+            id="full-name"
+            placeholder="Nome e Sobrenome"
+            required=""
+          />
 
-          <label class="label" for="full-name">Nome Completo</label>
-          <input class="input" type="text" name="name" id="full-name" placeholder="Nome e Sobrenome" required="" />
-          
-          <label class="label" for="email-address">Endereço de e-mail</label>
-          <input class="input" type="email" name="_replyto" id="email-address" placeholder="email@email.com" required="" />
+          <label class="label" for="email-address">
+            Endereço de e-mail
+          </label>
+          <input
+            class="input"
+            type="email"
+            name="_replyto"
+            id="email-address"
+            placeholder="email@email.com"
+            required=""
+          />
 
-          <label class="label" for="telephone">Telefone (Opicional)</label>
-          <input class="input" type="telephone" name="telephone" id="telephone" placeholder="(ddd) x xxxx-xxxx" />
+          <label class="label" for="telephone">
+            Telefone (Opicional)
+          </label>
+          <input
+            class="input"
+            type="telephone"
+            name="telephone"
+            id="telephone"
+            placeholder="(ddd) x xxxx-xxxx"
+          />
 
-          <label class="label" for="complaint">Descrição</label>
-          <textarea class="input" rows="6" name="Descrição da Ferramenta" id="complaint" placeholder="Descreve aqui informações relevantes sobre a ferramenta que deseja enviar, como: Nome; Área do conhecimento; Link para acesso; etc." required=""></textarea>
+          <label class="label" for="complaint">
+            Descrição
+          </label>
+          <textarea
+            class="input"
+            rows="6"
+            name="Descrição da Ferramenta"
+            id="complaint"
+            placeholder="Descreve aqui informações relevantes sobre a ferramenta que deseja enviar, como: Nome; Área do conhecimento; Link para acesso; etc."
+            required=""
+          ></textarea>
 
-          <button class="button" type="submit" disabled={state.submitting} value="Enviar" >Enviar</button>
+          <button
+            class="button"
+            type="submit"
+            disabled={state.submitting}
+            value="Enviar"
+          >
+            Enviar
+          </button>
         </form>
       </section>
     </div>
